04d5f6997864fb5d01594894d790588213cbd554
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-07-06 12:35:40 +0530

    read route basic done

---
+++ backend/allJsFun.js
@@ -0,0 +1,73 @@
+export function navAndInsert(arr, points, newItem) {
+    if (points.length == 1) {
+        const title = points[0].split('.')[0];
+        const quad = points[0].split('.')[1];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].title == title) {
+                arr[i][quad].push(newItem);
+                return;
+            }
+        }
+    } else {
+        const title = points[0].split('.')[0];
+        const quad = points[0].split('.')[1];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].title == title) {
+                points.shift();
+                return navAndInsert(arr[i][quad], points, newItem);
+            }
+        }
+    }
+}
+
+export function getAllTitles(obj, title) {
+    const iU = [];
+    for (let i = 0; i < obj.iu.length; i++) {
+        iU.push(obj.iu[i].title);
+    }
+
+    const iN = [];
+    for (let i = 0; i < obj.in.length; i++) {
+        iN.push(obj.in[i].title);
+    }
+
+    const nU = [];
+    for (let i = 0; i < obj.nu.length; i++) {
+        nU.push(obj.nu[i].title);
+    }
+
+    const nN = [];
+    for (let i = 0; i < obj.nn.length; i++) {
+        nN.push(obj.nn[i].title);
+    }
+
+    return {
+        title: title,
+        iU: iU,
+        iN: iN,
+        nU: nU,
+        nN: nN
+    }
+}
+
+export function navAndGet(arr, points) {
+    if (points.length == 1) {
+        if (points[0].split('.').length == 1) {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i].title == points[0]) {
+                    const result = getAllTitles(arr[i], arr[i].title);
+                    return result;
+                }
+            }
+        }
+    } else {
+        const title = points[0].split('.')[0];
+        const quad = points[0].split('.')[1];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].title == title) {
+                points.shift();
+                return navAndGet(arr[i][quad], points);
+            }
+        }
+    }
+}
--- backend/routes/todoRoute.js
+++ backend/routes/todoRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import passport from "passport";
 import { ToDo } from "../model/todoModel.js";
+import { getAllTitles, navAndGet, navAndInsert } from "../allJsFun.js";
 
 const router = express.Router();
 
-router.post("/add", passport.authenticate('jwt', { session: false }), async (req, res) => {
+router.post("/", passport.authenticate('jwt', { session: false }), async (req, res) => {
     try {
         let { path, text } = req.body;
         const currentUser = req.user;
@@ -47,31 +48,9 @@ router.post("/add", passport.authenticate('jwt', { session: false }), async (req
 
         const points = path.split("/");
 
-        function navAndInsert(arr, points, newItem) {
-            if(points.length == 1) {
-                const title = points[0].split('.')[0];
-                const quad = points[0].split('.')[1];
-                for(let i=0;i<arr.length;i++) {
-                    if(arr[i].title == title) {
-                        arr[i][quad].push(newItem);
-                        return;
-                    }
-                }
-            } else {
-                const title = points[0].split('.')[0];
-                const quad = points[0].split('.')[1];
-                for(let i=0;i<arr.length;i++) {
-                    if(arr[i].title == title) {
-                        points.shift();
-                        return navAndInsert(arr[i][quad], points, newItem);
-                    }
-                }
-            }
-        }
-
         const firstQ = points.shift();
-        
-        if(!points.length) {
+
+        if (!points.length) {
             toDo.todo[firstQ].push(item);
         } else {
             navAndInsert(toDo.todo[firstQ], points, item);
@@ -84,6 +63,56 @@ router.post("/add", passport.authenticate('jwt', { session: false }), async (req
         return res.status(201).send({
             message: "ToDo added successfully",
         });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({
+            message: error.message,
+        });
+    }
+});
+
+router.get("/", passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        let { path } = req.body;
+        const currentUser = req.user;
+        const userId = currentUser._id;
+
+        if (!path) {
+            return res.status(400).send({
+                message: "Send all the required data (path)",
+            });
+        }
+
+        const filter = { userId: userId };
+
+        let toDo = await ToDo.findOne(filter);
+
+        if (!toDo) {
+            return res.status(404).send({
+                message: "Your todo nest not found",
+            });
+        }
+
+        if (path == "home") {
+            const result = getAllTitles(toDo.todo, "Home");
+            return res.status(200).send(result);
+        }
+
+        const points = path.split("/");
+
+        const firstQ = points.shift();
+
+        if (!points.length) {
+            return res.status(400).send({
+                message: "wrong path is sent"
+            });
+        } else {
+            const result = navAndGet(toDo.todo[firstQ], points);
+            if(result) {
+                res.status(200).send(result);
+            }
+        }
 
     } catch (error) {
         console.log(error.message);
